6c9db76e9b3d80a59c801da86ccc3a493fed5dd6
Kit <74026992+aouwt@users.noreply.github.com>
2022-01-04 15:19:27 -0500

    .

--- irc.cpp
+++ irc.cpp
@@ -25,6 +25,19 @@ int IRC::send_msg (const char* what, const channel_t where) {
 		return IRC_CHANTOOLONG;
 
 	char msg [IRC_MESSAGELEN + 2];
-	if (snprintf (msg, IRC_MESSAGELEN, "PRIVMSG %s :%s", where, what) == EOF)
+	if (snprintf (msg, IRC_MESSAGELEN, "PRIVMSG %s :%s\r\n", where, what) == EOF)
 		return IRC_TOOLONG;
+
+	socket.send (msg);
+}
+
+int IRC::join_chan (const char* ch) {
+	if (strlen (ch) > IRC_CHANNELLEN)
+		return IRC_CHANTOOLONG;
+
+	char msg [IRC_MESSAGELEN + 2];
+	if (snprintf (msg, IRC_MESSAGELEN, "JOIN %s\r\n", ch) == EOF)
+		return IRC_TOOLONG;
+
+	socket.send (msg);
 }
