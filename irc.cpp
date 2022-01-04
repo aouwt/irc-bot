faf4505c8fcb6a3afee6f8e37fa4d3e80455aeee
aouwt <74026992+aouwt@users.noreply.github.com>
2022-01-03 20:25:43 -0500

    .

--- irc.cpp
+++ irc.cpp
@@ -3,7 +3,10 @@
 #include <Socks/ClientSocket.hpp>
 
 IRC::IRC (const char* domain, unsigned int port, const char* name) {
-	
+	if (socket.setSocket (domain, port)) {
+		err = IRC_CANTCONNECT;
+		return;
+	}
 }
 
 int IRC::send (const char* what) {
@@ -11,7 +14,7 @@ int IRC::send (const char* what) {
 	if (snprintf (msg, IRC_MESSAGELEN, "%s\n\r", what) == EOF)
 		return IRC_TOOLONG;
 	
-	
+	socket.send (msg);
 }
 
 int IRC::set_nick (const char* nick) {
